3aa16fcd5c04a12aba2081b2c31652a0a6d56ca4
Wangtomorrow <914606466@qq.com>
2019-04-14 19:36:01 +0800

    豆瓣爬虫1.1版本提交

---
+++ douban_prod.py
@@ -0,0 +1,87 @@
+import requests
+import time
+from pyquery import PyQuery as pq
+import re
+from urllib.parse import urlencode
+import csv
+
+base_url="https://movie.douban.com/subject/30163509/comments?"
+
+headers={
+    'Cookie':'**************************',
+}
+
+def save_to_csv(list,type,page):
+    str_param = type+'_'+str(page)
+    with open("F:/scrapyData/fcrs_%s.txt"%(str_param), "w",newline='',encoding='utf-8') as f:
+        for info in list:
+            #print(info)
+            f.writelines([info])
+            print(file=f)
+
+
+def parse_content(html):
+    doc = pq(html)  # 得到网页源码
+    contents = doc('.comment-item p').items()
+    scores = doc('.comment-item h3 .comment-info').items()
+    return contents,scores
+
+def get_page_html(page):
+
+    data={
+        'start':(page-1)*20,
+        'limit':20,
+        'status':'P'
+    }
+    queries=urlencode(data)
+    url=base_url+queries
+    html=get_html(url)
+    return html
+
+#解析html页面
+def get_html(url):
+    print('crawing ', url) # 输出正在爬取的url
+    response = requests.get(url,headers=headers)
+    time.sleep(2)  # 暂停10秒
+    return response.text
+
+#获取总的评论的条数
+def parse_comment(html):
+    doc=pq(html) # 得到网页源码
+    num=doc('#content > div > div.article > div.clearfix.Comments-hd > ul > li.is-active > span').text()#获取span标签的内容
+    num=re.findall('\d+',num)[0]
+    return num
+
+def main():
+    print("start")
+    index_html=get_html("https://movie.douban.com/subject/30163509/comments?status=P")
+    print(parse_comment(index_html))
+    total_num=parse_comment(index_html)
+    pagenum=int(total_num)/20+1
+    #for page in range(1, int(pagenum)):
+    for page in range(1, 150):
+        comment_list=[]
+        score_list=[]
+        print('page= '+str(page))
+        page_html=get_page_html(page)
+        if page_html:
+            contents,scores=parse_content(page_html)
+            for score in scores:
+                if( not str(score('span').eq(3)).strip()):
+                    score_deal = 'no score'
+                    print(score_deal)
+                else:
+                    score_deal = score('span').eq(2).attr('class')
+                    print(score_deal)
+                score_list = score_list + [score_deal]
+            for content in contents:
+                content_deal = content.text()
+                print(content_deal)
+                comment_list = comment_list + [content_deal]
+        save_to_csv(comment_list,'content',page)
+        save_to_csv(score_list,'score',page)
+
+
+
+if __name__=="__main__" :
+    main()
