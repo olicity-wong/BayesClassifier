498ad58713a1b797c9de18b9bcb37dd5f60b298d
Wangtomorrow <914606466@qq.com>
2019-04-14 23:47:53 +0800

    添加模拟登陆

--- douban_prod.py
+++ douban_prod.py
@@ -4,17 +4,47 @@ from pyquery import PyQuery as pq
 import re
 from urllib.parse import urlencode
 import datetime
+from http import cookiejar
 
 base_url="https://movie.douban.com/subject/30163509/comments?"
 
-user_agent = r'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36 Firefox/23.0'
-headers={
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive',
-    'User-Agnet': user_agent
+
+# headers_movie={
+#     'Connection': 'keep-alive',
+#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+#     'Accept-Encoding': 'gzip, deflate, br',
+#     'Accept-Language': 'zh-CN,zh;q=0.9',
+#     'Host': 'movie.douban.com',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36',
+#     'Content-Type': 'application/x-www-form-urlencoded',
+#     'Upgrade-Insecure-Requests:': '1',
+# }
+#
+# headers_login_start = {
+#     'Referer': 'https://accounts.douban.com/passport/login',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
+# }
+# headers_login_click={
+#     'Referer': 'https://accounts.douban.com/passport/login',
+#     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
+# }
+# headers_login_basic={
+#     'Accept': 'application/json',
+#     'Content-Type': 'application/x-www-form-urlencoded',
+#     'Origin': 'https://accounts.douban.com',
+#     'Referer': 'https://accounts.douban.com/passport/login',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36',
+#     'X-Requested-With': 'XMLHttpRequest',
+# }
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Referer':'https://accounts.douban.com/login?alias=&redir=https%3A%2F%2Fwww.douban.com%2F&source=index_nav&error=1001'
+
 }
+
+global session
+session=requests.Session()
+session.headers.update(headers)
 
 def save_to_csv(list,type,page):
     nowtime = datetime.datetime.now().strftime("%Y-%m-%d-%H_%M_%S") #获取当前时间
@@ -45,11 +75,11 @@ def get_page_html(page):
     html=get_html(url)
     return html
 
-#解析html页面
+#解析html页面session.headers.update(headers_movie)
 def get_html(url):
     print('crawing ', url) # 输出正在爬取的url
-    response = requests.get(url,headers=headers)
-    time.sleep(2)  # 暂停10秒
+    response = session.get(url,headers=headers)
+    time.sleep(15)  # 暂停10秒
     return response.text
 
 #获取总的评论的条数
@@ -65,8 +95,11 @@ def main():
     print(parse_comment(index_html))
     total_num=parse_comment(index_html)
     pagenum=int(total_num)/20+1
-    #for page in range(1, int(pagenum)):
-    for page in range(1, 6):
+    for page in range(1, int(pagenum)):
+        #10页登陆
+        if page%10==0:
+            douban_login()
+        print(session.cookies.items())
         comment_list=[]
         score_list=[]
         print('page= '+str(page))
@@ -89,6 +122,23 @@ def main():
         save_to_csv(score_list,'score',page)
 
 
+def douban_login():
+    username = '13031623728'
+    password = 'abc123456'
+    data = {                    #需要传去的数据
+        'ck':'B0hi',
+        'name':username,
+        'password':password,
+        'remember':'false',
+        'ticket':''
+    }
+    # login_start_url='https://www.douban.com/stat.html'
+    # login_click_url='https://www.douban.com/stat.html'
+    login_basic_url='https://accounts.douban.com/j/mobile/login/basic'
+
+    html = session.post(login_basic_url,data=data,headers=headers)
+    print(session.cookies.items())
 
 if __name__=="__main__" :
+    douban_login()
     main()
